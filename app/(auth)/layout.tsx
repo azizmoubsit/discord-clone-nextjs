e321ca611e7de76fb313e4a831ed51182bef98c4
azizmoubsit <azizmobsit@gmail.com>
2023-09-02 00:34:14 +0100

    add h-screen to auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div className="h-full flex justify-center items-center">{children}</div>;
+  return <div className="min-h-screen flex justify-center items-center">{children}</div>;
 };
 
 export default AuthLayout;
