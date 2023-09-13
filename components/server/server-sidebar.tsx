6fbbe2feb1189616700fe39a1071c445d084a54a
azizmoubsit <azizmobsit@gmail.com>
2023-09-13 01:21:03 +0100

    add command dialog to search for channels and members

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -20,7 +20,7 @@ const chennlelIconMap = {
 const roleIconMap = {
   [MemberRole.GUEST]: <Shield className="w-4 h-4" />,
   [MemberRole.ADMIN]: <ShieldCheck className="w-4 h-4" />,
-  [MemberRole.ADMIN]: <ShieldAlert className="w-4 h-4" />,
+  [MemberRole.MODERATOR]: <ShieldAlert className="w-4 h-4" />,
 };
 
 const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
@@ -57,51 +57,51 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   const members = server?.members.filter((member) => member.profileId !== profile.id);
   const role = server?.members.find((member) => member.profileId === profile.id)?.role;
 
-  const serverSearchData = [
-    {
-      label: "Text Channels",
-      type: "channel",
-      data: textChannels.map((channel) => ({
-        id: channel.id,
-        name: channel.name,
-        icon: chennlelIconMap[channel.type],
-      })),
-    },
-    {
-      label: "Audio Channels",
-      type: "channel",
-      data: audioChannels.map((channel) => ({
-        id: channel.id,
-        name: channel.name,
-        icon: chennlelIconMap[channel.type],
-      })),
-    },
-    {
-      label: "Video Channels",
-      type: "channel",
-      data: videoChannels.map((channel) => ({
-        id: channel.id,
-        name: channel.name,
-        icon: chennlelIconMap[channel.type],
-      })),
-    },
-    {
-      label: "Members",
-      type: "member",
-      data: members.map((member) => ({
-        id: member.id,
-        name: member.profile.name,
-        icon: roleIconMap[member.role],
-      })),
-    },
-  ];
-
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={server} role={role} />
       <ScrollArea className="flex-1 px-3">
         <div className="mt-2">
-          <ServerSearch data={serverSearchData} />
+          <ServerSearch
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannels.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: chennlelIconMap[channel.type],
+                })),
+              },
+              {
+                label: "Audio Channels",
+                type: "channel",
+                data: audioChannels.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: chennlelIconMap[channel.type],
+                })),
+              },
+              {
+                label: "Video Channels",
+                type: "channel",
+                data: videoChannels.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: chennlelIconMap[channel.type],
+                })),
+              },
+              {
+                label: "Members",
+                type: "member",
+                data: members.map((member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                })),
+              },
+            ]}
+          />
         </div>
       </ScrollArea>
     </div>
