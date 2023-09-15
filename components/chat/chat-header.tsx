8f896dc5c34cb1b6b19a117e6ceedbfc7c016b05
azizmoubsit <aziz.africaverify@gmail.com>
2023-09-15 12:33:01 +0100

    add conversation chat header

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 
 import MobileToggle from "@/components/mobile-toggle";
+import { UserAvatar } from "../user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -13,6 +14,7 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobileToggle serverId={serverId} />
+      {type === "converstaion" && <UserAvatar src={imageUrl} className="md:h-8 md:w-8 mr-2" />}
       {type === "channel" && <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
